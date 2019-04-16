f76ca3c4fce02384178b231007c995709b5f5c99
zero4994 <cavc94@gmail.com>
2019-04-16 18:54:14 +0900

    Adds state changed method and conditional rendering

--- src/App.js
+++ src/App.js
@@ -6,12 +6,37 @@ import "./App.css";
 require("dotenv").config();
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      access_token: "",
+      username: ""
+    };
+  }
+
+  handleAuthorizedUser = (access_token, username) => {
+    console.log(access_token, username);
+    this.setState({
+      access_token,
+      username
+    });
+    console.log("is state changed?", this.state);
+  };
+
   render() {
-    return (
-      <div className="App">
-        <Login />
-      </div>
-    );
+    let layout;
+    if (this.state.access_token && this.state.username) {
+      layout = (
+        <div>
+          <h1>Congratulations you are logged in</h1>
+        </div>
+      );
+    } else {
+      layout = <Login onUserAuthorized={this.handleAuthorizedUser} />;
+    }
+
+    return <div className="App">{layout}</div>;
   }
 }
 
