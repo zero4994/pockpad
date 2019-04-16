3f71ff4382ea884891b778a6721f2e51851e120a
zero4994 <cavc94@gmail.com>
2019-04-16 18:38:29 +0900

    Modifies resolver to return necessary data

--- server/resolvers/index.js
+++ server/resolvers/index.js
@@ -16,6 +16,30 @@ const resolvers = {
       console.log(error);
       return `${error.response.status} ${error.response.statusText}`;
     }
+  },
+  Authorize: async params => {
+    try {
+      console.log("Authorizing...");
+      const { data } = await axios({
+        method: "POST",
+        url: `https://getpocket.com/v3/oauth/authorize?consumer_key=${
+          process.env.CONSUMER_KEY
+        }&code=${params.code}`
+      });
+      console.log("This is the response ==>", data);
+
+      const userData = data.split("&").map(element => {
+        return element.toString().split("=")[1];
+      });
+
+      return {
+        access_token: userData[0],
+        username: userData[1]
+      };
+    } catch (error) {
+      console.log(error);
+      return `${error.response.status} ${error.response.statusText}`;
+    }
   }
 };
 
