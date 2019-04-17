251f007e44ae72bb39549f0c2f27e9caa630983b
zero4994 <cavc94@gmail.com>
2019-04-17 15:49:36 +0900

    Adds basic routing to project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import Login from "./components/Login";
 import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 require("dotenv").config();
 
@@ -25,18 +26,37 @@ class App extends Component {
   };
 
   render() {
-    let layout;
-    if (this.state.access_token && this.state.username) {
-      layout = (
-        <div>
-          <h1>Congratulations you are logged in</h1>
-        </div>
-      );
-    } else {
-      layout = <Login onUserAuthorized={this.handleAuthorizedUser} />;
-    }
+    // let layout;
+    // if (this.state.access_token && this.state.username) {
+    //   layout = (
+    //     <div>
+    //       <h1>Congratulations you are logged in</h1>
+    //     </div>
+    //   );
+    // } else {
+    //   layout = <Login onUserAuthorized={this.handleAuthorizedUser} />;
+    // }
 
-    return <div className="App">{layout}</div>;
+    // return <div className="App">{layout}</div>;
+    return (
+      <div className="App">
+        <Router>
+          <div>
+            {/* elements go here */}
+            <Switch>
+              <Route exact path="/" component={Login} />
+              <Route
+                path="/test"
+                component={() => (
+                  <div>Congratulations you have routed successfully</div>
+                )}
+              />
+              <Route exact path="*" component={Login} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
   }
 }
 
